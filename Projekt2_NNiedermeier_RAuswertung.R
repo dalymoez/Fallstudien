8804684bd384c08fbbc5e76b4283dd624028801a
NNiedermeier <49302672+NNiedermeier@users.noreply.github.com>
2023-10-26 20:56:57 +0200

    Add files via upload

---
+++ Projekt2_NNiedermeier_RAuswertung.R
@@ -0,0 +1,54 @@
+##################################################
+########## Fallstudien 1 Projekt 2 ##############
+################################################
+
+#Pakete
+library(ggplot2)
+library(ggplot)
+
+
+#Daten einladen
+
+Konzentration_data <- read.csv2("C:/Users/natha/Documents/XX", header= T, sep = ",")
+
+
+
+##########################################################################################
+##################### Bearbeitungszeiten untersuchen ##################################### 
+# Aufgabenstellung: Unterscheiden sich die Bearbeitungszeiten zwischen verschiedenen #####
+# Teilgruppen der Teilnehmer*innen? ######################################################
+##########################################################################################  
+
+
+
+
+
+
+#####################################################################################################
+########################Einfluss akustischer Störung ################################################
+# Aufgabenstellung: Ist der Konzentrationsscore niedriger, wenn die Konzentration durch #############
+# akustische Einflüsse gestört wird? Ist die Bearbeitungszeit höher, wenn die Konzentration durch ###
+# akustische Einflüsse gestört wird? ################################################################
+#####################################################################################################
+  
+
+
+
+
+###############################################################################################
+################# Akustische Störung im Detail ################################################
+# Aufgabenstellung: Unterscheidet sich der Einfluss der akustischen Einflüsse zwischen ######## 
+# den verschiedenen Teilgruppen der Teilnehmer*innen? #########################################
+###############################################################################################
+
+
+###############################################################################################
+############# Weitere Frage ###################################################################
+# Denken Sie sich als Gruppe mit uns eine weitere Fragestellungen für die Daten aus, ##########
+# die Sie mit geeigneten statistischen Inferenzverfahren beantworten wolle ####################
+###############################################################################################
+
+
+
+
+
